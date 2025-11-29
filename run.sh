e3fb6d14a7ed797c220373fabad759dfc0de06fb
edwardoboh <obohedward@gmail.com>
2025-11-29 19:33:58 +0100

    build: prepare stub generation workflow

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SERVICE_NAME=$1
+RELEASE_VERSION=$2
+
+sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+mkdir -p golang
+protoc --go_out=golang/ --go_opt=paths=source_relative \
+	--go-grpc_out=golang/ --go-grpc_opt=paths=source_relative \
+	${SERVICE_NAME}/*.proto
+cd golang/${SERVICE_NAME}
+go mod init github.com/edwardoboh/proto-template/golang/${SERVICE_NAME} || true
+go mod tidy
+cd ../../
+git config user.name "edwardoboh"
+git config user.email "obohedward@gmail.com"
+git add . && git commit -am "build: update .proto files" || true
+git tag -fa "golang/${SERVICE_NAME}/${RELEASE_VERSION}" \
+    -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
+git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
+
